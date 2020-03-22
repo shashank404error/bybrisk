501a18dccf51afe23bf65a018ecdd2fe286d6cbd
shashank prakash <shashanksharma191098@gmail.com>
2020-03-22 23:57:01 +0530

    worked on business connect

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET auth page. */
 router.get('/', function(req, res, next) {
-    res.render('auth', { title: 'Bybrisk | Login' });
+    res.render('auth',
+        { title: 'Bybrisk | Authorization',
+          auth: 'business partner',
+            link1:'Basic Info',
+            link2:'Getting started',
+        link3:'Launching',
+        link4:''});
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET auth page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bybrisk | Login' });
+  res.render('index', { title: 'Bybrisk | Login',
+  link1:'Our Services',
+  link2:'Contact',
+    link3:'',
+    link4:''});
 });
 
 
